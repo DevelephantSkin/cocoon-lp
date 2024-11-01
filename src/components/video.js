32de6d032622a8e5af59d93596778fa095b3bcbb
Galeno de Melo <galenodemelo@gmail.com>
2024-11-01 10:42:32 -0300

    fix: Changing language must change video subtitle

--- src/components/video.js
+++ src/components/video.js
@@ -1,15 +1,15 @@
-import { useRef, useEffect, useState } from 'react';
-import Image from 'next/image';
-import { motion, AnimatePresence } from 'framer-motion';
 import Player from '@vimeo/player';
+import { AnimatePresence, motion } from 'framer-motion';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
 import MotionTag from './animations/motionTag';
 
-import playBtn from '@/svg/play.svg';
 import { useLanguage } from '@/context/LanguageContext';
+import playBtn from '@/svg/play.svg';
 
 export default function Video({ videoId, cover }) {
-  const { language } = useLanguage()
+  const { language } = useLanguage();
   const ref = useRef(null);
   const [player, setPlayer] = useState(null);
   const [started, setStarted] = useState(false);
@@ -25,7 +25,6 @@ export default function Video({ videoId, cover }) {
         color: '744441',
         autopause: true,
         pip: true,
-        texttrack: language == 'pt' ? 'pt-BR' : 'en',
         playsinline: false,
       }),
     );
@@ -41,6 +40,7 @@ export default function Video({ videoId, cover }) {
             exit={{ opacity: 0 }}
             transition={{ duration: 1 }}
             onClick={() => {
+              player.enableTextTrack(language === 'pt' ? 'pt-BR' : 'en');
               player.play();
               setStarted(true);
             }}
