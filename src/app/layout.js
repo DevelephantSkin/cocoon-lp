f3b804e8d0ee5e2887276903ba86ed84761c0c59
Galeno de Melo <galenodemelo@gmail.com>
2024-11-15 09:49:49 -0300

    feat: Google Search Console verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" id="app">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="_Dypge19W8wzYwMCxIKpD_Ntku-ve_74Um2pz4HsLJQ"
+        />
+      </head>
       <ReCaptchaProvider reCaptchaKey="6LdGYhgqAAAAAJ5qUBZtUQPyDf7UlHryhrRmZazq">
         <body
           className={
