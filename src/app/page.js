e81b19b928f0f10ba77a0c430401ff3c463b98b4
Galeno de Melo <galenodemelo@gmail.com>
2024-11-22 15:26:45 -0300

    feat: On click, show full video

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,28 @@
 'use client';
 
-import Header from '@/components/header';
 import Footer from '@/components/footer';
+import Header from '@/components/header';
 
-import Cover from '@/sections/cover';
-import Milagres from '@/sections/milagres';
-import Diferenciais from '@/sections/diferenciais';
-import Unidades from '@/sections/unidades';
-import Design from '@/sections/design';
-import Implantacao from '@/sections/implantacao';
-import Facilidades from '@/sections/facilidades';
-import Contato from '@/sections/contato';
 import { LanguageProvider } from '@/context/LanguageContext';
+import Contato from '@/sections/contato';
+import Cover from '@/sections/cover';
+import Design from '@/sections/design';
+import Diferenciais from '@/sections/diferenciais';
+import Facilidades from '@/sections/facilidades';
+import Implantacao from '@/sections/implantacao';
+import Milagres from '@/sections/milagres';
+import Unidades from '@/sections/unidades';
+import { useState } from 'react';
 import ContactUs from '../components/contactUs';
+import PopupVideo from '../sections/popup-video';
 
 export default function Home() {
+  const [popupVideoOpened, setPopupVideoOpened] = useState(false);
+
   return (
     <LanguageProvider>
       <Header />
-      <Cover />
+      <Cover onButtonClick={() => setPopupVideoOpened(true)} />
       <main className="relative w-full max-w-full overflow-x-hidden p-8 pt-0 text-cacao lg:mb-16 lg:px-44">
         <Milagres />
         <Diferenciais />
@@ -28,6 +32,7 @@ export default function Home() {
         <Facilidades />
         <Contato />
       </main>
+      <PopupVideo opened={popupVideoOpened} setOpened={setPopupVideoOpened} />
       <Footer />
       <ContactUs />
     </LanguageProvider>
